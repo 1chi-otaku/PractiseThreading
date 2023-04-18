9cc06ef786fb171395e0f0c803bdac2e21a18d35
Serhii <mywaifuqueen@gmail.com>
2023-04-18 21:18:36 +0100

    Add project files.

---
+++ PractiseThreading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PractiseThreading
+{
+    internal class Program
+    {
+        struct ObjectArray
+        {
+            object[] array;
+            public void SetArray(object[] array)
+            {
+                this.array = array;
+            }
+            public void InitArray(int count)
+            {
+                this.array = new object[count];
+            }
+        }
+        public static void Thread(object array)
+        {
+            ObjectArray arr = (ObjectArray)array;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(arr.ToString());
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            ObjectArray objectArray= new ObjectArray();
+            objectArray.InitArray(10);
+            Thread th = new Thread(new ParameterizedThreadStart(Thread));
+            th.IsBackground = true;
+            th.Start(objectArray);
+        }
+    }
+}
---
+++ PractiseThreading2_/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PractiseThreading2_
+{
+    class Bank
+    {
+        private int money;
+        private string name;
+        private int percent;
+        public Bank(int money, string name, int percent)
+        {
+            this.money = money;
+            this.name = name;
+            this.percent = percent;
+        }
+        public void SetMoney(int money)
+        {
+            this.money = money;
+            Thread th = new Thread(new ThreadStart(FileWrite));
+            th.IsBackground = true;
+            th.Start();
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+            Thread th = new Thread(new ThreadStart(FileWrite));
+            th.IsBackground = true;
+            th.Start();
+        }
+        public void SetPercent(int percent)
+        {
+            this.percent = percent;
+            Thread th = new Thread(new ThreadStart(FileWrite));
+            th.IsBackground = true;
+            th.Start();
+        }
+        private void FileWrite()
+        {
+            StreamWriter writer = new StreamWriter("subj.txt", false);
+            writer.WriteLine("Money - " + money + "\nName - " + name + "\nPercent - "+ percent);
+            writer.Close();
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Bank bank = new Bank(500, "Privat", 15);
+                bank.SetMoney(800);
+                bank.SetPercent(12);
+                bank.SetName("Monobank");
+
+            }
+        }
+    }
+}
